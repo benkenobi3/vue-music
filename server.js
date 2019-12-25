56044ba731a5dec639116052a63bdd58067ef110
Max <32428314+benkenobi3@users.noreply.github.com>
2019-12-25 23:57:14 +0300

    history express

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
+
 app = express();
+
 app.use(serveStatic(__dirname + "/dist"));
+app.use(history())
+
 var port = process.env.PORT || 5000;
-app.listen(port);console.log('server started '+ port);
+
+app.listen(port);
+console.log('server started '+ port);

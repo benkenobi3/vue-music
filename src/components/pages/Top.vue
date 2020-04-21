193f0bc2856e4aa20fc108d63351880147807851
Max <32428314+benkenobi3@users.noreply.github.com>
2020-04-21 22:13:22 +0300

    change table to ul-li

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -2,11 +2,17 @@
   <div>
     <b-container fluid>
       <b-row class="showcase">
-        <b-col cols=11 offset=1>
+        <b-col cols=10 offset=1>
           <h1>Топ-10</h1>
           <ul id="top-list">
             <li v-for="album in topAlbums" :key="album.id">
-
+              <b-media>
+                <template v-slot:aside>
+                  <b-img width="64" alt="placeholder" :src="album.img_url"></b-img>
+                </template>
+                <h4>{{album.name}}</h4>
+                <p>{{album.singers[0].name}} • rate: {{album.rate}}</p>
+              </b-media>
             </li>
           </ul>
         </b-col>
@@ -31,5 +37,11 @@
 </script>
 
 <style lang="sass">
+
+  #top-list
+    list-style: none
+
+    li 
+      width: 100%
 
 </style>
